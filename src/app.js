12482edfc26ab8715675a7427304c47fcb157551
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-08-23 14:18:06 -0300

    trying new methods

--- src/app.js
+++ src/app.js
@@ -2,6 +2,57 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).json({ message: 'hello world!' }));
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/teams/:id', (req, res) => {
+  const team = teams.find((id) => Number(req.params.id) === id);
+  res.status(200).json({ team });
+});
+
+app.use(express.json());
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+/* app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200);
+}); */
 
 module.exports = app;
